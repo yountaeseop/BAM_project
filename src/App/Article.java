c96279c01b595b24e001545e5bc7deef90c9b8b7
yountaeseop <jangmj6931@gmail.com>
2022-08-02 14:12:14 +0900

    게시물 리스트 기능 구현

---
+++ src/App/Article.java
@@ -0,0 +1,15 @@
+package App;
+
+public class Article {
+	
+	int id;
+	String title;
+	String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+}
--- src/App/Main.java
+++ src/App/Main.java
@@ -1,11 +1,13 @@
 package App;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		ArrayList<Article> articles = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("==프로그램 시작==");
 		int id = 1;
@@ -15,15 +17,30 @@ public class Main {
 			String input = sc.nextLine().trim();
 			
 			if(input.equals("article list")) {
-				System.out.println("게시글이 없습니다");
+				System.out.printf("번호  /  제목\n");
+				
+				
+				
+				for(int i = 0; i < articles.size(); i++) {
+					int articleId = articles.get(i).id;
+					String articleTitle = articles.get(i).title;
+					
+					System.out.printf("%d %8s\n",articleId, articleTitle);
+					
+				}
+				
+				if(articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+				}
 				
 			} else if(input.equals("article write")) {
-				
 				
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
+				
+				articles.add(new Article(id, title, body));
 				
 				System.out.printf("%d번글이 생성되었습니다\n", id);
 				
