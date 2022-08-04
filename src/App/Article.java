81999b69c907ede6d3d99f7ca3ba2808e93676f5
yountaeseop <jangmj6931@gmail.com>
2022-08-04 16:45:32 +0900

    게시물 테스트 데이터 생성

--- src/App/Article.java
+++ src/App/Article.java
@@ -3,17 +3,22 @@ package App;
 public class Article {
 	
 	int id;
+	static int num;
 	String title;
 	String body;
 	String regDate;
 	int hit;
 
-	public Article(int id, String title, String body, String regDate, int hit) {
-		this.id = id;
+	public Article(String title, String body, String regDate) {
+		this.id = ++num; //id 자동증가
 		this.title = title;
 		this.body = body;
 		this.regDate = regDate;
-		this.hit = hit;
+		this.hit = 0;
+	}
+
+	public void increaseHit() {
+		hit++;
 	}
 
 //	public Article(int id, String title, String body) {
